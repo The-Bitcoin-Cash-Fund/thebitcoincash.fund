4be8ea73a93ab3ea0c25bb8698e480d5ee7faad3
Cameron McEwan <cameron@solomonpierce.com>
2018-01-17 11:46:58 +0900

    Tightened up the navbar

--- html/includes/mobile-nav.php
+++ html/includes/mobile-nav.php
@@ -1,14 +1,21 @@
 <div id="mobile-nav-panel">
   <nav role="navigation">
     <ul class="mobile-nav">
-      <li class="mobile-nav-item"><a href="<?= $config['base_url']; ?>about/">About</a></li>
       <li class="mobile-nav-item"><a href="<?= $config['base_url']; ?>proposal/">Submit a Proposal</a></li>
       <li class="mobile-nav-item"><a href="<?= $config['base_url']; ?>volunteer/">Volunteer</a></li>
       <li class="mobile-nav-item"><a href="<?= $config['base_url']; ?>donate/">Donate</a></li>
       <li class="mobile-nav-item"><a href="<?= $config['base_url']; ?>faqs/">FAQs</a></li>
+      <li class="mobile-nav-item"><a href="<?= $config['base_url']; ?>faqs/">About</a></li>
       <li class="mobile-nav-item">
-      <a href="<?= $config['base_url']; ?>contact/" title="Contact" id="mobile-nav-contact-link">Contact</a>
+        <a href="<?= $config['base_url']; ?>contact/" title="Contact" id="mobile-nav-contact-link">Contact</a>
       </li>
     </ul>
+    <!--
+    <ul class="mobile-nav-secondary">
+      <li class="mobile-nav-infoItem"><a href="<?= $config['base_url']; ?>about/">About</a></li>
+      <li class="mobile-nav-infoItem"><a href="<?= $config['base_url']; ?>about/">FAQs</a></li>
+      <li class="mobile-nav-infoItem"><a href="<?= $config['base_url']; ?>contact/">Contact</a></li>
+    </ul>
+    -->
   </nav>
 </div>
