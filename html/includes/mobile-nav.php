a8398a6f218b0e25c613402d340f998a6829d86c
Cameron McEwan <cameron@solomonpierce.com>
2018-02-05 17:24:54 +0900

    Added menuu options for language menu

--- html/includes/mobile-nav.php
+++ html/includes/mobile-nav.php
@@ -6,16 +6,39 @@
       <li class="mobile-nav-item"><a href="<?= $config['base_url']; ?>volunteer/">Volunteer</a></li>
       <li class="mobile-nav-item"><a href="<?= $config['base_url']; ?>donate/">Donate</a></li>
       <li class="mobile-nav-item"><a href="<?= $config['base_url']; ?>faqs/">FAQs</a></li>
+      <li class="mobile-nav-item"><a href="/" id="sn-btn-about">English
+          <div class="mobile-nav-expand-btn">
+            <span></span>
+            <span></span>
+          </div>
+        </a>
+        <ul class="mobile-nav mobile-subnav">
+          <li class="mobile-nav-item mobile-subnav-item item-closed sn-itm-about">
+            <a href="<?= $config['base_url']; ?>en/">English</a>
+          </li>
+          <li class="mobile-nav-item mobile-subnav-item item-closed sn-itm-about">
+            <a href="<?= $config['base_url']; ?>es/">Español</a>
+          </li>
+          <li class="mobile-nav-item mobile-subnav-item item-closed sn-itm-about">
+            <a href="<?= $config['base_url']; ?>it/">Italiano</a>
+          </li>
+          <li class="mobile-nav-item mobile-subnav-item item-closed sn-itm-about">
+            <a href="<?= $config['base_url']; ?>nl/">Nederlands</a>
+          </li>
+          <li class="mobile-nav-item mobile-subnav-item item-closed sn-itm-about">
+            <a href="<?= $config['base_url']; ?>pt-PT/">Português Portugal</a>
+          </li>
+          <li class="mobile-nav-item mobile-subnav-item item-closed sn-itm-about">
+            <a href="<?= $config['base_url']; ?>ja/">日本語</a>
+          </li>
+          <li class="mobile-nav-item mobile-subnav-item item-closed sn-itm-about">
+            <a href="<?= $config['base_url']; ?>zh/">中文</a>
+          </li>
+        </ul>
+      </li>
       <li class="mobile-nav-item">
         <a href="<?= $config['base_url']; ?>contact/" title="Contact" id="mobile-nav-contact-link">Contact</a>
       </li>
     </ul>
-    <!--
-    <ul class="mobile-nav-secondary">
-      <li class="mobile-nav-infoItem"><a href="<?= $config['base_url']; ?>about/">About</a></li>
-      <li class="mobile-nav-infoItem"><a href="<?= $config['base_url']; ?>about/">FAQs</a></li>
-      <li class="mobile-nav-infoItem"><a href="<?= $config['base_url']; ?>contact/">Contact</a></li>
-    </ul>
-    -->
   </nav>
 </div>
