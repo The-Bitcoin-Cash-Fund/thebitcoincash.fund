a23e4511f877e0b9f6507b96266bbcf77028f6fc
Cameron McEwan <cameron@solomonpierce.com>
2018-02-05 17:23:51 +0900

    Moved mobile nav menu button to the right

--- html/includes/navbar-xs.php
+++ html/includes/navbar-xs.php
@@ -1,5 +1,10 @@
 <!-- Mobile Navigation Bar -->
 <div id="navbar-xs" class="hidden-md hidden-lg">
+  <div id="navbar-xs-brand">
+    <a href="<?= $config['base_url']; ?>" title="The Bitcoin Cash Fund">
+      <img src="<?= $config['img_dir']; ?>img-logo-sm.png" alt="The Bitcoin Cash Fund" class="img-responsive">
+    </a>
+  </div>
   <button id="navbar-xs-menu-toggle" class="btn">
     <div id="burger-menu">
       <span></span>
@@ -8,16 +13,5 @@
       <span></span>
     </div>
   </button>
-  <div id="navbar-xs-brand">
-    <a href="<?= $config['base_url']; ?>" title="The Bitcoin Cash Fund">
-      <img src="<?= $config['img_dir']; ?>img-logo-sm.png" alt="The Bitcoin Cash Fund" class="img-responsive">
-    </a>
-  </div>
-  <!--
-  <a href="/cart" id="navbar-xs-cart-toggle" class="btn">
-    <img id="ico-cart-xs" src="//cdn.shopify.com/s/files/1/2446/5431/t/4/assets/ico-cart-xs.svg?4763073960845624351">
-    <img id="ico-cart-xs-active" src="//cdn.shopify.com/s/files/1/2446/5431/t/4/assets/ico-cart-xs-active.svg?4763073960845624351" class="hidden">
-  </a>
-  -->
   <div class="clearfix"></div>
 </div>
